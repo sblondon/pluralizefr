1dcac59409fbffa63b9a027b558e97ac897c4184
Stéphane Blondon <stephane.blondon@gmail.com>
2016-09-10 13:18:26 +0200

    rename README to README.rst for proper rendering in Github; reuse the file for long description of the package

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 setup(
         name="pluralizefr",
         description="pluralize for french language",
+        long_description=open("README.rst").read(),
         version="0.0.1",
         url="https://github.com/sblondon/pluralizefr",
         packages=[
